c06509a33480641c366813fdbfbd0e7c111bdf01
Swetha3221 <swethagummagari@gmail.com>
2024-10-03 20:51:30 +1200

    sumoftuple.py

--- sumoftuple.py
+++ sumoftuple.py
@@ -11,3 +11,24 @@ Sample Output:
 '''
 '''
 Ans:
+# Input: number of values to be entered
+n = int(input("Enter the number of values: "))
+
+# Initialize an empty list to store the values
+values = []
+
+# Loop to get n values from the user
+for _ in range(n):
+    value = int(input())  # Get each value
+    values.append(value)  # Add the value to the list
+
+# Create a tuple from the list of values
+values_tuple = tuple(values)
+
+# Calculate the sum of the tuple values using the predefined sum() method
+total_sum = sum(values_tuple)
+
+# Print the sum of the values
+print(total_sum)
+'''
+'''
