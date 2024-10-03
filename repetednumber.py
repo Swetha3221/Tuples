8612e6bfdbe2cfa9d859f73f383ef96c3fd66b77
Swetha3221 <swethagummagari@gmail.com>
2024-10-03 20:45:10 +1200

    repetednumber.py

--- repetednumber.py
+++ repetednumber.py
@@ -4,8 +4,26 @@
 Sample Input:
 1 2 3 1 2 3 4 1 2 1
 1
+Sample Output:
+4
+
 '''
 '''
 Ans:
+# Input: a string of space-separated numbers
+input_values = input("Enter tuple values: ")
+
+# Split the input string into individual numbers and convert them to integers
+numbers_tuple = tuple(map(int, input_values.split()))
+
+# Input: the value to count
+x = int(input("Enter the value to count: "))
+
+# Count the occurrences of x in the tuple
+count_x = numbers_tuple.count(x)
+
+# Print the result
+print(count_x)
+  
 '''
 '''
