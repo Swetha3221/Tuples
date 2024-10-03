4319372c876414cf49b407b53fbdcccd975b2004
Swetha3221 <swethagummagari@gmail.com>
2024-10-03 20:48:12 +1200

    minimumoftuple.py

--- minimumoftuple.py
+++ minimumoftuple.py
@@ -11,5 +11,30 @@ Sample Output:
 '''
 '''
 Ans:
+# Input: number of values to be entered
+n = int(input())
+
+# Initialize an empty list to store the values
+values = []
+
+# Loop to get n values from the user
+for _ in range(n):
+    value = int(input())  # Get each value
+    values.append(value)  # Add the value to the list
+
+# Create a tuple from the list of values
+values_tuple = tuple(values)
+
+# Initialize min_value with the first element of the tuple
+min_value = values_tuple[0]
+
+# Loop through the tuple to find the actual minimum value
+for number in values_tuple:
+    if number < min_value:
+        min_value = number  # Update min_value if a smaller number is found
+
+# Print the minimum value
+print(min_value)
+
 '''
 '''
